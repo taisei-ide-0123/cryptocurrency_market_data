fc29ad46c7e0cfbda80db3ee016d1d3894cc37aa
taisei0123 <taisei8955@gmail.com>
2021-07-11 15:26:50 +0900

    fetch data 100 of crypto currencies

--- coingecko_next/pages/index.tsx
+++ coingecko_next/pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.data);
   return (
     <div className={styles.container}>
       <Head>
@@ -20,5 +22,22 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=jpy&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  );
+  const data: {} = await res.json();
+
+  try {
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
